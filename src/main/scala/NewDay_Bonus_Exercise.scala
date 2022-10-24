79902c431d2423af20215979a8b8279c54ca56ce
tefnaminta <tefnaminta@gmail.com>
2022-10-24 09:22:31 +0100

    new

--- src/main/scala/NewDay_Bonus_Exercise.scala
+++ src/main/scala/NewDay_Bonus_Exercise.scala
@@ -8,7 +8,7 @@ object NewDay_Bonus_Exercise extends NewDay_dataframes {
 
     val topthree_movies = spark.sql("select * from (select UserID, Title, Rating, row_number() over (partition by UserID order by Rating desc) as ranknum from mr_df)rnk where ranknum <=3")
     val top3movies_df = topthree_movies.groupBy("UserID").agg(collect_list("Title").name("Movie_Names"))
-    top3movies_df.coalesce(1).write.option("header", "true").json("./results/top3movies_result")
+    top3movies_df.coalesce(1).write.option("header", "true").parquet("./results/top3movies_result")
     top3movies_df.show(5)
   }
 
--- src/main/scala/NewDay_Main_Exercise.scala
+++ src/main/scala/NewDay_Main_Exercise.scala
@@ -6,9 +6,9 @@ object NewDay_Main_Exercise extends NewDay_dataframes {
     val movie_rating_result = spark.sql("SELECT Title, MAX(Rating) as MaxRating, MIN(Rating) as MinRating, AVG(Rating) as AverageRating FROM mr_df group by Title")
     movie_rating_result.show(5)
 
-    movies_df.coalesce(1).write.option("header", "true").json("./results/movie_data")
-    ratings_df.coalesce(1).write.option("header", "true").json("./results/rating_data")
-    movie_rating_result.coalesce(1).write.option("header", "true").json("./results/movie_rating_result")
+    movies_df.coalesce(1).write.option("header", "true").parquet("./results/movie_data")
+    ratings_df.coalesce(1).write.option("header", "true").parquet("./results/rating_data")
+    movie_rating_result.coalesce(1).write.option("header", "true").parquet("./results/movie_rating_result")
   }
 
 }
